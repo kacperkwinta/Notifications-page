6ad83f7b31a4847b6fc463dc7500e0dc27419105
turbobaguettes <kaspersky.kwinta@gmail.com>
2023-04-14 12:03:49 +0200

    ✨ clearing notifications mechanics added

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
 "use strict";
 console.log(123);
+
+const notificationsNumber = document.querySelector(".notifications-number");
+const btnClearNotifications = document.querySelector(".mark-as-read");
+const notificationsDot = document.querySelectorAll(".dot");
+
+btnClearNotifications.addEventListener("click", function () {
+	notificationsNumber.textContent = "0";
+
+	notificationsDot.forEach((dot) => {
+		dot.classList.add("dot--hidden");
+	});
+});
